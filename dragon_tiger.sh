91f87b79d69874cd92e31c652df15a235accf81f
flyinghorse0510 <mhy@flyinghorse.top>
2023-07-13 13:28:26 +0700

    modify dragon_tiger.sh

--- dragon_tiger.sh
+++ dragon_tiger.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./run x --no-audio --stay-awake -b 20M --max-fps=30 --tcpip=192.168.0.17
+./run x --no-audio --stay-awake -b 20M --max-fps=5 --tcpip=192.168.0.17 --v4l2-sink=/dev/video0
